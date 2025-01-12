4657e96f74b6f263f7d01c8bc9e23890e0b0185d
ali <ali.ergul@pusula.net.tr>
2025-01-12 22:28:31 +0300

    minor responsive updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ function App() {
   return (
     <>
       <Menu variant="horizontal" current={0}>
+        <MenuItem>Anasayfa</MenuItem>
         <MenuItem>Hakkımda</MenuItem>
         <MenuItem>İletişim</MenuItem>
         <MenuItem>Blog</MenuItem>
         <MenuItem>Makaleler</MenuItem>
         <MenuItem>Dersler</MenuItem>
+        <MenuItem>Kaynaklar</MenuItem>
       </Menu>
 
       <Menu variant="horizontal-filled" current={1}>
+        <MenuItem>Anasayfa</MenuItem>
         <MenuItem>Hakkımda</MenuItem>
         <MenuItem>İletişim</MenuItem>
         <MenuItem>Blog</MenuItem>
         <MenuItem>Makaleler</MenuItem>
         <MenuItem>Dersler</MenuItem>
+        <MenuItem>Kaynaklar</MenuItem>
       </Menu>
 
       <Menu variant="vertical" current={2}>
+        <MenuItem>Anasayfa</MenuItem>
         <MenuItem>Hakkımda</MenuItem>
         <MenuItem>İletişim</MenuItem>
         <MenuItem>Blog</MenuItem>
         <MenuItem>Makaleler</MenuItem>
         <MenuItem>Dersler</MenuItem>
+        <MenuItem>Kaynaklar</MenuItem>
       </Menu>
     </>
   );
